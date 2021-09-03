ad0afac93806aab3a8f64d1c351afc80d5235aa6
2021-1-RV <ysukata@gmail.com>
2021-09-03 21:09:06 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+//Соединить два массива в один без повторов (без одинаковых повторяющихся элементов)
+
+function connect(arr) {
+  var a = arr.concat()
+  for (var i = 0; i < a.length; ++i) {
+    for (var j = i + 1; j < a.length; ++j) {
+      if (a[i] === a[j]) a.splice(j--, 1)
+    }
+  }
+
+  return a
+}
+
+const first = [1, 3, 4, 5, 7, 8, 11, 12, 14, 15]
+const second = [1, 2, 4, 6, 7, 9, 10, 12, 13, 15]
+const third = connect(first.concat(second))
+
+console.log(third)
